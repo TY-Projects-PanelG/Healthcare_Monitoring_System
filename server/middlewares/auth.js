5238578c872249ab4cd250e53bc22f0ef26f1ff6
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-08-15 15:18:09 +0530

    middlewares

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,7 +2,17 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (request,response,next)=>{
     try {
-        
+        let token = request.header("Authorization");
+
+        if(!token) return response.status(403).send("Access Denied!");
+
+        if(token.startsWith('Bearer ')){
+            token = token.slice(7, token.length).trimLeft();
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        request.patient = verified;
+        next();
     } catch (error) {
         console.log(error);
     }
