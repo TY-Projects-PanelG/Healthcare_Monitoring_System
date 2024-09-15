2c97f807b08f27dab9f99bf7ea677fce11cbed93
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-09-15 20:23:56 +0530

    video call functionality backend

---
+++ server/controllers/videoCall.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+io.on("connection", (socket) => {
+    console.log("A user connected: ", socket.id);
+
+    // Send the user's ID to the client
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded");
+    });
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", {
+            signal: data.signalData,
+            from: data.from,
+            name: data.name
+        });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`App listening on Port: ${PORT}`);
+});
